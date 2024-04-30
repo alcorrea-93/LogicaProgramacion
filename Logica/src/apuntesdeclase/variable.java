b26f71ba3f11c4f549c56e847323b8dfbf63e0c8
Alejandro Correa G <132523061+alcorrea-93@users.noreply.github.com>
2024-04-30 17:48:33 -0500

    Primera clase Java
    Variables y formulario

---
+++ Logica/src/apuntesdeclase/variable.java
@@ -0,0 +1,29 @@
+
+package apuntesdeclase;
+
+
+public class variable {
+     
+    public static void main(String[] args) {
+
+        
+        String autorName = "Alejo";
+      
+        System.out.println("Hola " + autorName);
+    }
+    
+   /* tipos de datos: 
+    
+byte
+short
+int 
+long
+
+float
+double
+
+char
+
+boolean 
+*/
+}
---
+++ Logica/src/ejercicios/formulario.java
@@ -0,0 +1,21 @@
+
+
+package ejercicios;
+
+
+public class formulario {
+         public static void main(String[] args) {
+        
+        int id = 1;
+        String Nombre = "Alejo";
+        String Apellido  ="Correa";
+        char genero = 'm';
+        long telefono = 301791288l;
+        String corre = "acorrea1@cesde.net";
+        double salario = 1052465;
+        float estatura = 1.72f;
+        boolean estado = true;
+        
+       System.out.println("Nombre del usuario: " + Nombre + "\nApellido del usuario: " + Apellido + "\nGénero del usuario: " + genero + "\nCorreo electrónico del usuario: " + corre + "\nSalario del usuario: " + salario + "\nEstatura del usuario: " + estatura + "\nEstado del usuario: " + estado + "\nTeléfono del usuario: " + telefono);
+    }
+}
