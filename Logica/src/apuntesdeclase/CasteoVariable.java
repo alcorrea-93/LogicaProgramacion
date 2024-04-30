2a39daa08205e53e76bd990d954bc2631a328fd7
Alejandro Correa G <132523061+alcorrea-93@users.noreply.github.com>
2024-04-30 17:56:52 -0500

    Con casteo

---
+++ Logica/src/apuntesdeclase/CasteoVariable.java
@@ -0,0 +1,16 @@
+
+package apuntesdeclase;
+
+public class CasteoVariable {
+    public static void main(String[] args) {
+        byte nivel = 127;
+        short nivel1 = (short) nivel;
+        System.out.println(nivel1);
+        
+        nivel1 = 130;
+        
+        System.out.println(nivel1);
+    }
+    
+    //ejercicio: castear a cada tipo de dato numérico siendo nivel2 int y así
+}
